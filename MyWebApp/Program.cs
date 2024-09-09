86d7581c6b61a695df2cc77919eddb2c6a03a143
ayeshanaaz1103 <ayeshanaaz1103@gmail.com>
2024-09-08 23:32:37 -0400

    Update Program.cs

--- MyWebApp/Program.cs
+++ MyWebApp/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Ayesha, This is your first Azure Devops CI/CD Project ");
+app.MapGet("/", () => "Hello Ayesha, Introducing your first Azure Devops CI/CD Project ");
 
 app.Run();
